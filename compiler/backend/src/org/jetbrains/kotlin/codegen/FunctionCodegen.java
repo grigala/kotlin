16c51cecb747bbb39c432fdc5195a3898470e581
Natalia Ukhorskaya <natalia.ukhorskaya@jetbrains.com>
2015-09-28 16:43:02 +0300

    Write local variable for inline function and inline argument

--- compiler/backend/src/org/jetbrains/kotlin/codegen/FunctionCodegen.java
+++ compiler/backend/src/org/jetbrains/kotlin/codegen/FunctionCodegen.java
@@ -36,6 +36,7 @@ import org.jetbrains.kotlin.descriptors.annotations.Annotated;
 import org.jetbrains.kotlin.descriptors.annotations.AnnotationDescriptor;
 import org.jetbrains.kotlin.descriptors.annotations.AnnotationUseSiteTarget;
 import org.jetbrains.kotlin.jvm.RuntimeAssertionInfo;
+import org.jetbrains.kotlin.load.java.JvmAbi;
 import org.jetbrains.kotlin.load.java.JvmAnnotationNames;
 import org.jetbrains.kotlin.load.kotlin.nativeDeclarations.NativeDeclarationsPackage;
 import org.jetbrains.kotlin.name.FqName;
@@ -193,7 +194,7 @@ public class FunctionCodegen {
                     getThisTypeForFunction(functionDescriptor, methodContext, typeMapper),
                     new Label(),
                     new Label(),
-                    contextKind
+                    methodContext
             );
 
             mv.visitEnd();
@@ -373,7 +374,7 @@ public class FunctionCodegen {
         mv.visitLabel(methodEnd);
 
         Type thisType = getThisTypeForFunction(functionDescriptor, context, typeMapper);
-        generateLocalVariableTable(mv, signature, functionDescriptor, thisType, methodBegin, methodEnd, context.getContextKind());
+        generateLocalVariableTable(mv, signature, functionDescriptor, thisType, methodBegin, methodEnd, context);
     }
 
     private static void generateLocalVariableTable(
@@ -383,13 +384,13 @@ public class FunctionCodegen {
             @Nullable Type thisType,
             @NotNull Label methodBegin,
             @NotNull Label methodEnd,
-            @NotNull OwnerKind ownerKind
+            @NotNull MethodContext context
     ) {
         Iterator<ValueParameterDescriptor> valueParameters = functionDescriptor.getValueParameters().iterator();
         List<JvmMethodParameterSignature> params = jvmMethodSignature.getValueParameters();
         int shift = 0;
 
-        boolean isStatic = AsmUtil.isStaticMethod(ownerKind, functionDescriptor);
+        boolean isStatic = AsmUtil.isStaticMethod(context.getContextKind(), functionDescriptor);
         if (!isStatic) {
             //add this
             if (thisType != null) {
@@ -420,6 +421,23 @@ public class FunctionCodegen {
             Type type = param.getAsmType();
             mv.visitLocalVariable(parameterName, type.getDescriptor(), null, methodBegin, methodEnd, shift);
             shift += type.getSize();
+        }
+
+        if (context.isInlineFunction()) {
+            mv.visitLocalVariable(JvmAbi.LOCAL_VARIABLE_NAME_PREFIX_INLINE_FUNCTION + functionDescriptor.getName(),
+                                  Type.INT_TYPE.getDescriptor(), null, methodBegin, methodEnd, shift);
+        }
+
+        if (context.isInliningLambda()) {
+            if (thisType != null) {
+                String name = thisType.getClassName();
+                int indexOfLambdaOrdinal = name.lastIndexOf("$");
+                if (indexOfLambdaOrdinal > 0) {
+                    String lambdaOrdinal = name.substring(indexOfLambdaOrdinal + 1);
+                    mv.visitLocalVariable(JvmAbi.LOCAL_VARIABLE_NAME_PREFIX_INLINE_ARGUMENT + lambdaOrdinal,
+                                          Type.INT_TYPE.getDescriptor(), null, methodBegin, methodEnd, shift);
+                }
+            }
         }
     }
 
